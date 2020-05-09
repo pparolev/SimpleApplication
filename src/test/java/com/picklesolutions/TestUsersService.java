aae87b1d8876c049ff2ff745fac8ab15f253a910
George Dimitrov <george.dimitrov@gmail.com>
2020-05-09 11:54:43 +0300

    tt

--- src/test/java/com/picklesolutions/TestUsersService.java
+++ src/test/java/com/picklesolutions/TestUsersService.java
@@ -1,7 +1,8 @@
 package com.picklesolutions;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestUsersService {
 
@@ -10,5 +11,15 @@ public class TestUsersService {
         UsersService usersService = new UsersService();
 
         assertEquals(10, usersService.count());
+        assertNotEquals(11, usersService.count());
+    }
+
+    @Test
+    public void testAuthentication() {
+        UsersService usersService = new UsersService();
+
+        assertTrue(usersService.AuthenticateUser("admin", "adminpass"));
+        assertFalse(usersService.AuthenticateUser("admin", "adminpass2"));
+        assertFalse(usersService.AuthenticateUser("admin2", "adminpass2"));
     }
 }
